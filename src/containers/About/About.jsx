bf65bd1027154c0242846b907a960b70524f2a05
ppratham0811 <ppratham0811@gmail.com>
2022-08-07 17:40:32 +0530

    About section, transitions added

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -5,25 +5,6 @@ import "./About.scss";
 import { urlFor, client } from "../../Client";
 import {AppWrapper} from "../../wrapper/";
 
-
-// const abouts = [
-//   {
-//     title: "Fullstack Web Developer",
-//     description: "I love web development",
-//     imgUrl: images.about01
-//   },
-//   {
-//     title: "AI/ML Developer",
-//     description: "B. Tech in CSE with Specialization in AI/ML",
-//     imgUrl: images.about02
-//   },
-//   {
-//     title: "Blockchain Enthusiast",
-//     description: "Very interested in blockchain development, currently learning it",
-//     imgUrl: images.about03
-//   }
-// ]
-
 const About = () => {
   const [abouts, setAbouts] = useState([]);
 
@@ -35,18 +16,27 @@ const About = () => {
 
   return (
     <>
-      <motion.div id="about" className="app__about-header" whileInView={{x:[1000,0]}} viewport={{once: true}}>
-        <span>About </span>me
+      <motion.div className="app__about-header" whileInView={{x:[1000,0]}} transition={{duration: 1}} viewport={{once: true}}>
+        <h1 className="head-text"><span>About</span> Me</h1>
+      </motion.div>
+      <motion.div className="app__about-desc" whileInView={{opacity:[0,1]}} transition={{duration: 1}} viewport={{once: true}}>
+        <h3 style={{marginBottom: 10}}>Who I am?</h3>
+        <p className="p-text">I am a B. Tech student at Vellore Institute of Technology, Chennai. I am pursuing Computer science and engineering with specialization in AI and ML. I love doing web development. I spend my time on learning, doing projects and Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, fugiat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum exercitationem iste voluptas. Voluptate quos nesciunt quidem recusandae qui. Error, quaerat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta rem suscipit et omnis ab est molestias sit quis labore. Asperiores hic exercitationem, accusamus veniam aliquid molestias explicabo placeat. Dolor, excepturi?</p>
+      </motion.div>
+
+      <motion.div style={{marginTop: 40}} whileInView={{x:[-1000,0]}} transition={{duration: 1}} viewport={{once: true}}>
+        <h2 className="head-text">What I <span>Love to do?</span></h2>
       </motion.div>
       <div className="app__profiles">
         {abouts.reverse().map((about, index) => {
           return (
             <motion.div
-              whileInView={{ opacity: 1 }}
+              whileInView={{ opacity: [0,1]}}
               whileHover={{ scale: 1.1 }}
-              transition={{ duration: 0.5, type: "tween" }}
+              transition={{ duration: 1, type: "tween" }}
               className="app__profile-item"
               key={about.title + index}
+              viewport={{once: true}}
             >
               <img src={urlFor(about.imgUrl)} alt={about.title} />
               <h2 className="bold-text" style={{ marginTop: 20 }}>
@@ -62,4 +52,4 @@ const About = () => {
 };
 
 export default AppWrapper(About, 'about');
-// export default AppWrapper(About, 'about');
+// export default About;
