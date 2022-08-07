c63af537a4625e0835b2e9200675f8907df4d402
ppratham0811 <ppratham0811@gmail.com>
2022-08-07 17:36:31 +0530

    app wrapper fixed component render error

--- src/wrapper/AppWrapper.js
+++ src/wrapper/AppWrapper.js
@@ -9,12 +9,6 @@ const AppWrapper = (Component, idName, classNames) =>
         <div className={`app__container ${classNames}`} id={idName}>
           <div className="app__wrapper app__flex">
             <Component />
-            {/* <div className="copyright">
-              <p className="p-text">
-                {currentYear}&copy;Prathamesh Potabatti&trade;
-              </p>
-              <p className="p-text">All Rights Reserved</p>
-            </div> */}
           </div>
           <NavigationDots active={idName} />
         </div>
