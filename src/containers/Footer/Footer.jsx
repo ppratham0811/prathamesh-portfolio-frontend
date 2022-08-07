809476aaf740333ec9c45fa1039982899ff789ca
ppratham0811 <ppratham0811@gmail.com>
2022-08-07 17:37:35 +0530

    Footer section

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -3,7 +3,15 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <>Footer</>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <>
+      <footer className="app__footer-sticky">
+        <p className="p-text">{`${currentYear}-${currentYear+1}`}&copy; Prathamesh Potabatti</p>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
