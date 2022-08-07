cbd80b8f65ec349627d3db15393ef39602ce20c8
ppratham0811 <ppratham0811@gmail.com>
2022-08-07 17:38:02 +0530

    added new images imports

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,8 +3,7 @@ import mobile from "../assets/mobile.png";
 import api from "../assets/api.png";
 import cpp from "../assets/cpp.png";
 import css from "../assets/css.png";
-import figma from "../assets/figma.png";
-import flutter from "../assets/flutter.png";
+import figma from "../assets/figma.svg";
 import git from "../assets/git.png";
 import github from "../assets/github.svg";
 import linkedin from "../assets/linkedin.png";
@@ -13,14 +12,11 @@ import instagram from "../assets/instagram.svg";
 import graphql from "../assets/graphql.png";
 import html from "../assets/html.png";
 import javascript from "../assets/javascript.png";
-import mu5 from "../assets/mu5.png";
 import node from "../assets/node.png";
 import python from "../assets/python.png";
 import react from "../assets/react.png";
-import redux from "../assets/redux.png";
 import sass from "../assets/sass.png";
 import typescript from "../assets/typescript.png";
-import vue from "../assets/vue.png";
 import about01 from "../assets/about01.png";
 import about02 from "../assets/about02.png";
 import about03 from "../assets/about03.png";
@@ -43,7 +39,6 @@ const images = {
   cpp,
   css,
   figma,
-  flutter,
   git,
   github,
   linkedin,
@@ -52,14 +47,11 @@ const images = {
   graphql,
   html,
   javascript,
-  mu5,
   node,
   python,
   react,
-  redux,
   sass,
   typescript,
-  vue,
   about01,
   about02,
   about03,
