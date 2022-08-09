68de78b412b1912b22fa432f496ab8c0b12654cc
ppratham0811 <ppratham0811@gmail.com>
2022-08-09 16:06:21 +0530

    skills section added popups

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import ReactTooltip from "react-tooltip";
+import ReactTooltip from "react-tooltip";
 import { motion } from "framer-motion";
 
 import { urlFor, client } from "../../Client";
@@ -22,20 +22,23 @@ const Skills = () => {
       <motion.h2 className="head-text" whileInView={{x:[1000,0]}} transition={{duration: 1}} viewport={{once: true}}>My <span>Skills</span></motion.h2>
       <motion.div className="app__skills-desc" whileInView={{opacity:[0,1]}} transition={{duration: 1}} viewport={{once: true}}>
         <h3 style={{marginBottom: 10}}>What I do?</h3>
-        <p className="p-text">I am good at web development and machine learning. I build websites in the MERN stack. I am also good with other technologies such as git, figma, scss, bootstrap, tailwind etc. I put good efforts in optimizing the code, so that websites run efficiently. I always strive to make the functionalities better. I have used SQL and NoSQL based databases. I also have experience in AI/ML related works. I use Python for AI/ML and DL related tasks. I am also interested in blockchain technology, and I am currently learning more about it.</p>
+        <p className="p-text">I am good at fullstack web development and machine learning. I build websites in the MERN stack. I am also good with other technologies such as git, figma, scss, bootstrap, tailwind etc. I put good efforts in optimizing the code, so that websites run efficiently. I always strive to make the functionalities better. I have used SQL and NoSQL based databases. I also have experience in AI/ML related works. I use Python for AI/ML and DL related tasks. I am also interested in blockchain technology, and I am currently learning more about it.</p>
       </motion.div>
       <div className="app__skills-container">
         <motion.div className="app__skills-list" whileInView={{opacity:[0, 1]}} transition={{duration: 1}} viewport={{once: true}}>
           {skills.map((skill, index) => {
             return (
-              <div className="app__skills">
+              <div className="app__skills" key={`skill-${index}`}>
                 <div
+                  data-tip
+                  data-for={skill.name}
                   key={index}
                   className="app__skills-icons"
                 >
                   <img src={urlFor(skill.icon)} alt={`${skill.name}-icon`} />
                 </div>
-                <p className="p-text">{skill.name}</p>
+                <ReactTooltip id={skill.name} effect="solid" arrowColor="#000" className="skills-tooltip">{skill.name}</ReactTooltip>
+                <p className="p-text skills-tooltip-p">{skill.name}</p>
               </div>
             );
           })}
