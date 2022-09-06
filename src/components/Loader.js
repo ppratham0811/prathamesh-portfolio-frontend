f749cd476aba5e6bd81085182c05ccbf4499d432
ppratham0811 <ppratham0811@gmail.com>
2022-09-06 23:51:18 +0530

    need to add a custom loader

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import CircularProgress from "@mui/material/CircularProgress";
+import "../index.css";
+
+const CircleLoader = () => {
+  return (
+    <div className="circle">
+      <CircularProgress />
+    </div>
+  );
+};
+
+export default CircleLoader;
