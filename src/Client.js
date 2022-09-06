39ea9792fa883dab5b8df115f30a40d0514da104
ppratham0811 <ppratham0811@gmail.com>
2022-09-06 23:50:43 +0530

    fixed sanity connection issues

--- src/Client.js
+++ src/Client.js
@@ -1,12 +1,12 @@
-import sanityClient from "@sanity/client";
+import SanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+export const client = SanityClient({
+  projectId: "mkgfcbh2",
   dataset: "portfoliodataset",
-  apiVersion: "2022-08-01",
+  apiVersion: "2022-08-11",
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: "sk902XycJelVBwOQ2Nh1B9BoIjVcWQ3bi5Zgz1aRXbd8Y3XJFBi589j5eEczkxMb8XOTcAFf8tZYr6wkOduIoB0vucLeDtIAOl7jetXmh6DQICz5j9wFDU4ieT6hQPzXi6FlIP4syo367TnJYs20Q42QdBqasFDRt6LNePAJTAFFUKATz7j3",
 });
 
 const builder = imageUrlBuilder(client);
